66b0a335c35c391ca1dd1dfe056ce3555d86ced1
tlewiswilliams <43961115+tlewiswilliams@users.noreply.github.com>
2019-09-30 12:51:02 -0500

    Add files via upload

---
+++ ArrayReverse.java
@@ -0,0 +1,12 @@
+public class ArrayReverse {
+    static int[] reverse(int a[], int n)
+    {
+        int[] b = new int[n];
+        int j = n;
+        for (int i = 0; i < n; i++) {
+            b[j - 1] = a[i];
+            j = j - 1;
+        }
+        a = b;
+    }
+}
